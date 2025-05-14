f47b5678955a665c4aea26e680b6f7c7eec21c83
JeongJinbong <jbong0104@naver.com>
2025-05-14 17:06:23 +0900

    이벤트로 받은 포인트 추가하고 멤버 points에 추가

--- app/src/main/java/kr/ac/tukorea/ge/and/jjb/smoothingpath/PathView.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/jjb/smoothingpath/PathView.java
@@ -2,12 +2,15 @@ package kr.ac.tukorea.ge.and.jjb.smoothingpath;
 
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class PathView extends View {
     private static final String TAG = PathView.class.getSimpleName();
@@ -25,11 +28,15 @@ public class PathView extends View {
     private void init(){
     }
 
+    private ArrayList<PointF> points = new ArrayList<>();
+
+
     public boolean onTouchEvent(MotionEvent event)
     {
         float x = event.getX();
         float y = event.getY();
-        Log.d("TouchEvent : action =" + event.getAction() + "pos=" + x + "," + y);
+        points.add(new PointF(x,y));
+        Log.d(TAG, "TouchEvent: action=" + event.getAction() + " pos=" + x + "," + y + " now points count=" + points.size());
         return super.onTouchEvent(event);
     }
 }
